c89e9098b44747940d5a9eecd35f2ae6f6b08370
raunifabricio <raunifabricio10@gmail.com>
2021-09-06 17:40:13 -0300

    first commit

---
+++ product-catalog/src/main/java/one/digitalinnovation/experts/productcatalog/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.experts.productcatalog.repository;
+
+import one.digitalinnovation.experts.productcatalog.model.Product;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+public interface ProductRepository extends ElasticsearchRepository<Product, Integer> {
+}
+
---
+++ shopping-cart/src/main/java/one/digitalinnovation/experts/shoppingcart/repository/CartRepository.java
@@ -0,0 +1,8 @@
+package one.digitalinnovation.experts.shoppingcart.repository;
+
+import one.digitalinnovation.experts.shoppingcart.model.Cart;
+import org.springframework.data.repository.CrudRepository;
+
+
+public interface CartRepository extends CrudRepository<Cart, Integer> {
+}
